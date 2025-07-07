80db688c7d6d4c222d35bfc54a49cd4fd7ec58a7
dachi712 <dachinovator@gmail.com>
2025-07-07 20:01:55 +0400

    feat: added husky

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const salami = 'dd'
+
+
+
+    
+    console.log('saal');
+    
     return 'Hello World!';
   }
 }
