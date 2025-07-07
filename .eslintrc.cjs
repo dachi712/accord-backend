6aab1ddab4c2e7ea5befaef23e8bab86886c6439
dachi712 <dachinovator@gmail.com>
2025-07-07 19:51:25 +0400

    feat: testing console log

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.build.json'],
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2024: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    eqeqeq: ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+};
